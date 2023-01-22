12c91442334e491d08f5416fbe21a335507dbf43
Brandon Hyungjun Cho <brandon.hyungjun.cho@ibm.com>
2023-01-22 14:21:42 -0800

    adding form

--- todolistd/src/Components/TodoForm.js
+++ todolistd/src/Components/TodoForm.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
 export default class TodoForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            inputText : ""
+        }
+    }
+    handleInput = (event) => {
+        this.setState({
+            inputText: event.target.value
+        });
+    }
     render () {
         return (
-            <input type="text" />
+            <input 
+                type="text" 
+                value={this.state.inputText}
+                onChange={this.handleInput}
+                placeholder="todo..."
+            />
         );
     }
 }
