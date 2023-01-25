3f231e3fe7219b313bff8068591102731eaec23d
Brandon Hyungjun Cho <brandon.hyungjun.cho@ibm.com>
2023-01-24 23:50:53 -0800

    adding delete funcitonality

--- todolistd/src/App.js
+++ todolistd/src/App.js
@@ -18,6 +18,18 @@ class App extends React.Component {
     });
   }
 
+  deleteToDos = (id) => {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id!==id)
+    });
+  }
+
+  deleteCompletedToDos = () => {
+    this.setState({
+        todos: this.state.todos.filter(todo => !todo.isCompleted)
+    });
+  }
+  
   toggleComplete = (id) => {
     this.setState({
       todos: this.state.todos.map(todo=>{
@@ -55,12 +67,14 @@ class App extends React.Component {
           <TodoList 
             todo={todo}
             key={todo.id}
-            toggleComplete={()=>{this.toggleComplete(todo.id)}} 
+            toggleComplete={()=>{this.toggleComplete(todo.id)}}
+            handleDelete={()=>{this.deleteToDos(todo.id)}} 
           />
         ))}
         <button onClick={()=>{this.showTodos("all")}}>All</button>
         <button onClick={()=>{this.showTodos("active")}}>Active</button>
         <button onClick={()=>{this.showTodos("completed")}}>Completed</button>
+        <button onClick={this.deleteCompletedToDos}>Delete Completed</button>
       </div>
     );
   }
--- todolistd/src/Components/TodoLists.js
+++ todolistd/src/Components/TodoLists.js
@@ -7,11 +7,14 @@ export default class TodoList extends React.Component {
     render () {
         const {isCompleted} = this.props.todo;
         return (
-            <div 
-                onClick={this.props.toggleComplete}
-                style={{textDecoration: isCompleted ? "line-through" : ""}}
-            >
-                {this.props.todo.text}
+            <div style={{display:"flex", justifyContent:"center"}}>            
+                <div 
+                    onClick={this.props.toggleComplete}
+                    style={{textDecoration: isCompleted ? "line-through" : ""}}
+                >
+                    {this.props.todo.text}
+                </div>
+                <button onClick={this.props.handleDelete}>x</button>
             </div>
         );
     }
