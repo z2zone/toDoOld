3ffa0f645c81314f6eba4e5415d9770dc66f5a9e
hjun1114@gmail.com <brandon.hyungjun.cho@ibm.com>
2023-03-17 23:02:57 -0700

    practice

--- todolistd/src/App.js
+++ todolistd/src/App.js
@@ -3,63 +3,87 @@ import TodoForm from "./Components/TodoForm";
 import TodoList from "./Components/TodoLists";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      todos: []
-    }
-  }
-  addToDos = (todo) => {
-    this.setState({
-      todos: [...this.state.todos, todo]
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			todos: [],
+		};
+	}
+	addToDos = (todo) => {
+		this.setState({
+			todos: [...this.state.todos, todo],
+		});
+	};
 
-  deleteToDos = (id) => {
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id!==id)
-    });
-  }
+	deleteToDos = (id) => {
+		this.setState({
+			todos: this.state.todos.filter((todo) => todo.id !== id),
+		});
+	};
 
-  deleteCompletedToDos = () => {
-    this.setState({
-        todos: this.state.todos.filter(todo => !todo.isCompleted)
-    });
-  }
-  
-  toggleComplete = (id) => {
-    this.setState({
-      todos: this.state.todos.map(todo=>{
-        if(todo.id === id) {
-          return {
-            ...todo,
-            isCompleted: !todo.isCompleted
-          }
-        }else{
-          return todo;
-        }
-      })
-    });
-  }
-  render() {
-    return (
-      <div className="App">
-        <TodoForm addToDos={this.addToDos} />
-        {this.state.todos.map(todo => (
-          <TodoList 
-            todos={todo}
-            key={todo.id}
-            toggleComplete={()=>{this.toggleComplete(todo.id)}}
-            handleDelete={()=>{this.deleteToDos(todo.id)}} 
-          />
-        ))}
-        <button onClick={()=>{this.showTodos("all")}}>All</button>
-        <button onClick={()=>{this.showTodos("active")}}>Active</button>
-        <button onClick={()=>{this.showTodos("completed")}}>Completed</button>
-        <button onClick={this.deleteCompletedToDos}>Delete Completed</button>
-      </div>
-    );
-  }
+	deleteCompletedToDos = () => {
+		this.setState({
+			todos: this.state.todos.filter((todo) => !todo.isCompleted),
+		});
+	};
+
+	toggleComplete = (id) => {
+		this.setState({
+			todos: this.state.todos.map((todo) => {
+				if (todo.id === id) {
+					return {
+						...todo,
+						isCompleted: !todo.isCompleted,
+					};
+				} else {
+					return todo;
+				}
+			}),
+		});
+	};
+	render() {
+		return (
+			<div className="App">
+				<TodoForm addToDos={this.addToDos} />
+				{this.state.todos.map((todo) => (
+					<TodoList
+						todos={todo}
+						key={todo.id}
+						toggleComplete={() => {
+							this.toggleComplete(todo.id);
+						}}
+						handleDelete={() => {
+							this.deleteToDos(todo.id);
+						}}
+					/>
+				))}
+				<button
+					onClick={() => {
+						this.showTodos("all");
+					}}
+				>
+					All
+				</button>
+				<button
+					onClick={() => {
+						this.showTodos("active");
+					}}
+				>
+					Active
+				</button>
+				<button
+					onClick={() => {
+						this.showTodos("completed");
+					}}
+				>
+					Completed
+				</button>
+				<button onClick={this.deleteCompletedToDos}>
+					Delete Completed
+				</button>
+			</div>
+		);
+	}
 }
 
 export default App;
