6c451c63a670864ff40182ad347543c85118059f
hjun1114@gmail.com <brandon.hyungjun.cho@ibm.com>
2023-03-18 23:33:10 -0700

    practice

--- todolistd/src/Components/TodoForm.js
+++ todolistd/src/Components/TodoForm.js
@@ -1,32 +1,36 @@
-import React from 'react';
+import React from "react";
 
 export default class TodoForm extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            inputText : ""
-        }
-    }
-    handleInput = (event) => {
-        this.setState({
-            inputText: event.target.value
-        });
-    }
-    handleSubmit = (event) => {
-        event.preventDefault();
-        let todo = {id: new Date().valueOf(), text: this.state.inputText, isCompleted: false}
-        this.props.addToDos(todo);
-    }
-    render () {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input 
-                    type="text" 
-                    value={this.state.inputText}
-                    onChange={this.handleInput}
-                    placeholder="todo..."
-                />
-            </form>
-        );
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			inputText: "",
+		};
+	}
+	handleInput = (event) => {
+		this.setState({
+			inputText: event.target.value,
+		});
+	};
+	handleSubmit = (event) => {
+		event.preventDefault();
+		let todo = {
+			id: new Date().valueOf(),
+			text: this.state.inputText,
+			isCompleted: false,
+		};
+		this.props.addToDos(todo);
+	};
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input
+					type="text"
+					value={this.state.inputText}
+					onChange={this.handleInput}
+					placeholder="todo..."
+				/>
+			</form>
+		);
+	}
 }
--- todolistd/src/Components/TodoLists.js
+++ todolistd/src/Components/TodoLists.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 
 export default class TodoList extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    render () {
-        return (
-            <div style={{display:"flex", justifyContent:"center"}}>            
-                <div 
-                    onClick={this.props.toggleComplete}
-                    style={{textDecoration: isCompleted ? "line-through" : ""}}
-                >
-                    {this.props.todo.text}
-                </div>
-                <button onClick={this.props.handleDelete}>x</button>
-            </div>
-        );
-    }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<div style={{ display: "flex", justifyContent: "center" }}>
+				<div
+					onClick={this.props.toggleComplete}
+					style={{
+						textDecoration: isCompleted ? "line-through" : "",
+					}}
+				>
+					{this.props.todo.text}
+				</div>
+				<button onClick={this.props.handleDelete}>x</button>
+			</div>
+		);
+	}
 }
